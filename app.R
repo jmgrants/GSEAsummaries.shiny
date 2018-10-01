0f5a78c00f7e0bc1ac32886227f5850833d5a59a
jmgrants <jgrants@gphost02.bcgsc.ca>
2018-10-01 15:21:27 -0700

    commit of working app!

--- app.R
+++ app.R
@@ -8,34 +8,43 @@ ui <- fluidPage(
   # Sidebar with input for data and user options 
   sidebarLayout(
     sidebarPanel(
-      fileInput(inputId = "gsea.data", label = "GSEA Report (.csv format)"),
-      br(), br(),
+      print(strong("1) Input your file:")),
       
-      numericInput(inputId = "NES.max", label = "Max. NES Cutoff (for Upregulated pathways)", value = 1.5),
+      fileInput(inputId = "gsea.data", label = "GSEA Report (must be .csv format)"),
+
+      print(strong("2) Specify if up/downregulated & cutoff:")),
+      
+      checkboxInput(inputId = "below.zero", label = "NESs < 0?", value = FALSE),
+      
+      numericInput(inputId = "NES.cut", label = "NES Cutoff", value = 1.5),
       br(),
-      numericInput(inputId = "NES.min", label = "Min. NES Cutoff (for Downregulated pathways)", value = -1.5)
+      
+      print(strong("3) Manipulate downloaded plot:")),
+
+      numericInput(inputId = "text.size", label = "Text Size", value = 14),
+      
+      numericInput(inputId = "width", label = "Width of Downloaded Plot (in.)", value = 8),
+      
+      numericInput(inputId = "height", label = "Height of Downloaded Plot (in.)", value = 4),
+      
+      downloadButton("download.plot", "Download Plot")
     ),
     
     # Main panel output
     mainPanel(
       
       # Preview of user data
-      print("Preview of your data:"),
+      print(strong("Preview of your data:")),
       br(),
       tableOutput("prev.table"),
       br(),
       
-      # NES/FDR plot based on user-defined cutoff for UPREGULATED
-      print("NES & FDR of top upregulated gene sets"),
+      # NES/FDR plot based on user-defined cutoff
+      print(strong("NES & FDR of deregulated gene sets surpassing cutoff:")),
       br(), br(),
-      plotOutput("plot.up"),
-      br(), br(), br(), br(),
+      plotOutput("plot.data"),
+      br(), br(), br(), br()
       
-      
-      # NES/FDR plot based on user-defined cutoff for DOWNREGULATED
-      print("NES & FDR of top downregulated gene sets"),
-      br(), br(),
-      plotOutput("plot.down")
     )
   )
 )
@@ -47,45 +56,88 @@ server <- function(input, output, session) {
     read.csv(input$gsea.data$datapath)
   })
   
-  #output$prev.table <- renderTable({
-  #  dat.table() %>% setNames(c("name", "msigdb", "details", "size", "ES", "NES", "pval", "pfdr", "pfwer", "rank", "leading_edge")) %>% head(4)
-  #})
+  output$prev.table <- renderTable({
+    dat.table() %>% 
+      setNames(c("name", "msigdb", "details", "size", "ES", "NES", "pval", "pfdr", "pfwer", "rank", "leading_edge")) %>%
+      select(-(msigdb:details)) %>%
+      head(4)
+  })
   
-  # NES/FDR plot based on user-defined cutoff for UPREGULATED
-#  max.cut <- input$NES.max
+  # NES/FDR plot based on user-defined cutoff
+
+  output$plot.data <- renderPlot({
+    
+    if(input$below.zero == FALSE) {
+    
+    dat.table() %>%
+      setNames(c("name", "msigdb", "details", "size", "ES", "NES", "pval", "pfdr", "pfwer", "rank", "leading_edge")) %>%
+      filter(NES > input$NES.cut) %>% 
+      ggplot(aes(reorder(name, NES), NES, fill = pfdr)) +
+      geom_col() +
+      scale_fill_gradient(low = "blue4", high = "slategray1") +
+      labs(x = "", fill = "FDR") +
+      coord_flip() +
+      theme_bw(base_size = input$text.size) +
+      theme(axis.text = element_text(colour = "black"),
+            legend.key.size = unit(0.5, "cm"),
+            legend.margin = margin(0,0,0,-2))
+    } else {
+      dat.table() %>%
+        setNames(c("name", "msigdb", "details", "size", "ES", "NES", "pval", "pfdr", "pfwer", "rank", "leading_edge")) %>%
+        filter(NES < input$NES.cut) %>% 
+        ggplot(aes(reorder(name, NES), NES, fill = pfdr)) +
+        geom_col() +
+        scale_fill_gradient(low = "blue4", high = "slategray1") +
+        labs(x = "", fill = "FDR") +
+        coord_flip() +
+        theme_bw(base_size = input$text.size) +
+        theme(axis.text = element_text(colour = "black"),
+              legend.key.size = unit(0.5, "cm"),
+              legend.margin = margin(0,0,0,-2))
+    }
+    })
   
- # output$plot.up <- renderPlot({
-#    expr = dat.table() %>%
-#      setNames(c("name", "msigdb", "details", "size", "ES", "NES", "pval", "pfdr", "pfwer", "rank", "leading_edge")) %>%
-#      filter(NES > max.cut) %>% 
-#      ggplot(aes(reorder(name, NES), NES, fill = pfdr)) +
-#      geom_col() +
-#      scale_fill_gradient(low = "blue4", high = "slategray1") +
-#      labs(x = "", fill = "FDR") +
-#      coord_flip() +
-#      theme_bw(base_size = 8) +
-#      theme(axis.text = element_text(colour = "black"),
-#            legend.key.size = unit(0.3, "cm"),
-#            legend.margin = margin(0,0,0,-2))
-#    })
   
-  # NES/FDR plot based on user-defined cutoff for DOWNREGULATED
-#  min.cut <- input$NES.min
+  # For the download button
+  plot.to.save <- reactive({
+   
+     if(input$below.zero == FALSE) {
+      
+      dat.table() %>%
+        setNames(c("name", "msigdb", "details", "size", "ES", "NES", "pval", "pfdr", "pfwer", "rank", "leading_edge")) %>%
+        filter(NES > input$NES.cut) %>% 
+        ggplot(aes(reorder(name, NES), NES, fill = pfdr)) +
+        geom_col() +
+        scale_fill_gradient(low = "blue4", high = "slategray1") +
+        labs(x = "", fill = "FDR") +
+        coord_flip() +
+        theme_bw(base_size = input$text.size) +
+        theme(axis.text = element_text(colour = "black"),
+              legend.key.size = unit(0.5, "cm"),
+              legend.margin = margin(0,0,0,-2))
+    } else {
+      dat.table() %>%
+        setNames(c("name", "msigdb", "details", "size", "ES", "NES", "pval", "pfdr", "pfwer", "rank", "leading_edge")) %>%
+        filter(NES < input$NES.cut) %>% 
+        ggplot(aes(reorder(name, NES), NES, fill = pfdr)) +
+        geom_col() +
+        scale_fill_gradient(low = "blue4", high = "slategray1") +
+        labs(x = "", fill = "FDR") +
+        coord_flip() +
+        theme_bw(base_size = input$text.size) +
+        theme(axis.text = element_text(colour = "black"),
+              legend.key.size = unit(0.5, "cm"),
+              legend.margin = margin(0,0,0,-2))
+    }
+  })
   
-#  output$plot.up <- renderPlot({
- #   expr = dat.table() %>%
-  #    setNames(c("name", "msigdb", "details", "size", "ES", "NES", "pval", "pfdr", "pfwer", "rank", "leading_edge")) %>%
-  #    filter(NES < min.cut) %>% 
-  #    ggplot(aes(reorder(name, -NES), NES, fill = pfdr)) +
-  #    geom_col() +
-  #    scale_fill_gradient(low = "blue4", high = "slategray1") +
-  #    labs(x = "", fill = "FDR") +
-  #    coord_flip() +
-  #    theme_bw(base_size = 8) +
-  #    theme(axis.text = element_text(colour = "black"),
-  #          legend.key.size = unit(0.3, "cm"),
-  #          legend.margin = margin(0,0,0,-2))
-  #  })
+  output$download.plot <- downloadHandler(
+    filename = "GSEA_summary.png",
+    content = function(file) {
+      ggsave(file, plot = plot.to.save(), device = "png", dpi = 300, width = input$width, height = input$height)
+    }
+  )
+  
 }
   
   
